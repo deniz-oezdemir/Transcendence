902740ab676c1eedde58a009278c5b7d14a603f3
carina <carinaestevezorth@gmail.com>
2025-01-18 03:04:17 +0100

    added automated API documentation

--- user-access-management/django_project/settings.py
+++ user-access-management/django_project/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts",
     "rest_framework",
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
