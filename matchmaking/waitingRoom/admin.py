fa5479be8de03ca8f5da716f002174bdf4ff241b
deniz <oezdemir.de@gmail.com>
2025-01-31 12:38:57 +0100

    feat: add static file handling and integrate Font Awesome icons in admin interface

--- matchmaking/waitingRoom/admin.py
+++ matchmaking/waitingRoom/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-
-from .models import Match
+from .models import Match, Tournament
 
 admin.site.register(Match)
+admin.site.register(Tournament)
