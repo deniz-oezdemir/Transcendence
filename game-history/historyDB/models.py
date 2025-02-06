60be5ddc874e900ca65b4bb808f0eb986ce04a3b
migmanu <jmanuelmigoya@gmail.com>
2025-02-06 15:47:32 +0100

    feat(models): add FinishedGame model
    Added FinishedGame model to store details of completed games, including
    player IDs, names, and scores. This will help in tracking game history
    and player performance.

--- game-history/historyDB/models.py
+++ game-history/historyDB/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class FinishedGame(models.Model):
+    player_1_id = models.IntegerField(null=True, blank=True)
+    player_2_id = models.IntegerField(null=True, blank=True)
+    player_1_name = models.CharField(max_length=100)
+    player_2_name = models.CharField(max_length=100)
+    player_1_score = models.IntegerField(default=0)
+    player_2_score = models.IntegerField(default=0)
